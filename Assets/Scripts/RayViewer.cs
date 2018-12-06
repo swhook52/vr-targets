086ccdfc290e347cdb5d64a4700f868490fb54b1
Steven Hook <steven@hookscode.com>
2018-12-05 19:22:28 -0500

    Add raycast shooting scripts

---
+++ Assets/Scripts/RayViewer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class RayViewer : MonoBehaviour
+{
+    public float weaponRange = 50f;
+    public GameObject gunEnd;
+
+    void Start()
+    {
+    }
+
+
+    void Update()
+    {
+        // Draw a line in the Scene View  from the point lineOrigin in the direction of fpsCam.transform.forward * weaponRange, using the color green
+        Debug.DrawRay(gunEnd.transform.position, gunEnd.transform.forward * weaponRange, Color.green);
+    }
+}
---
+++ Assets/Scripts/ShootableTarget.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ShootableTarget : MonoBehaviour
+{
+    public int currentHealth = 3;
+
+    public void Damage(int damageAmount)
+    {
+        //subtract damage amount when Damage function is called
+        currentHealth -= damageAmount;
+
+        //Check if health has fallen below zero
+        if (currentHealth <= 0)
+        {
+            //if health has fallen below zero, deactivate it
+            gameObject.SetActive(false);
+        }
+    }
+}
